3e0f89142c55f8f0e3bd01e7129644b45e3a0771
Ziqi Fan <fanxx234@node0001.localdomain>
2017-01-15 22:31:32 -0600

    modified analyze.cpp

--- analysis/analyze.cpp
+++ analysis/analyze.cpp
@@ -3,6 +3,8 @@
  * This app designed for analysis large amount of results from CDBB experiments
  * The inputFile contains the results wanted to be analyzed
  * e.g. ./analysis file.txt
+ * sample line in file.txt: 
+ *     $$ CKPT Run 3: Elapsed time for writer rank 9 is 2.279051, timeStart 1484497521.737813, timeEnd 1484497524.016864
  * */
 
 #include <iostream>
@@ -23,14 +25,39 @@ int main(int argc, char *argv[]) {
 
     string lineInFile;
     size_t found;
+    
+    double result[20][1000];
+    
     if(myfile.is_open()) {
         while(myfile.good()) {
-            getline (myfile,lineInFile);
+            getline(myfile,lineInFile);
             found = lineInFile.find("$$");
             if(found != string::npos) {
-                cout<<lineInFile<<endl;
+                // get the number of CKPT Run 
+                size_t position = lineInFile.find("CKPT Run ");
+                string substring = lineInFile.substr(position+9);
+                int CKPT = atoi(substring.c_str());
+                
+                // get the number of writer rank
+                position = lineInFile.find("writer rank ");
+                substring = lineInFile.substr(position+12);
+                int writer = atoi(substring.c_str());
+                
+                // get the number of elapsed time
+                position = lineInFile.find("is ");
+                substring = lineInFile.substr(position+3);
+                double time = atof(substring.c_str());
+                
+                result[CKPT][writer] = time;
             }
         }
+    }
+    
+    for(int i=0; i<5; i++) {
+        for(int j=0; j<40; j++) {
+            cout<<result[i][j]<<" ";
+        }
+        cout<<endl;
     }
 
     return 0;
